953949a60e0fa9b40b7bf57cb2d343e2ac270c7e
celic <cceli@codequarry.net>
2014-03-19 13:16:41 -0400

    Adds port reading

--- server.rb
+++ server.rb
@@ -1,1 +1,12 @@
-puts "Damian likes penis"
+# Store command line port numbers
+port_numbers = Array.new
+
+abort("Not enough arguments!") if ARGV.length < 1
+
+# Store arguments in port_numbers
+ARGV.each do |arg|
+	arg_num = arg.to_i
+	port_numbers << arg_num if (arg_num > 0 && arg_num < 65536)
+end
+
+port_numbers.each{|port| puts port}
