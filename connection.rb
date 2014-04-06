e97c14fbbad8291a557cf91abd4f65d688d0bd11
Damian Mastylo <dmastylo@gmail.com>
2014-04-06 18:28:14 -0400

    Random messages

--- connection.rb
+++ connection.rb
@@ -1,10 +1,12 @@
 class Connection
 
-  attr_accessor :nick_name, :processing_message, :receivers
+  attr_accessor :nick_name, :processing_message, :receivers, :message_count, :recent_senders
 
   def initialize(nick_name, client)
     @nick_name = nick_name
     @client = client
+    @message_count = 0
+    @recent_senders = []
   end
 
   def reset_status
